09c87147d079851cac4252925bce2e88a67b4c15
Aaron Tong <luvxinc@gmail.com>
2026-03-31 02:59:18 -0700

    fix: bump min_version to 1.1.0

--- server/go/config/config.go
+++ server/go/config/config.go
@@ -9,7 +9,7 @@ import (
 
 // MinClientVersion is the minimum client version the server will accept.
 // Bump this whenever a client update should be enforced.
-const MinClientVersion = "1.0.9"
+const MinClientVersion = "1.1.0"
 
 type DatabaseConfig struct {
 	URL      string `yaml:"url"`
